7c233621c3c793f879c7a7aab8cbee0d698ed8a5
Hoon H <drawtree@me.com>
2014-10-12 15:23:02 +0900

    Updated Swift Bootstrapping example to be more simpler.

--- ComponentUsages/Bootstrapping/main.swift
+++ ComponentUsages/Bootstrapping/main.swift
@@ -10,10 +10,28 @@ import AppKit
 
 
 
+final class ExampleApplicationController: NSObject, NSApplicationDelegate {
+	
+	///	Seems fine to create AppKit UI classes before `NSApplication` object
+	///	to be created starting OSX 10.10. (it was an error in OSX 10.9)
+	let	window1	=	NSWindow()
+	
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: true)
+		window1.makeKeyAndOrderFront(self)
+	}
+	
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+	
+}
+
+
 
 ///	When compared to Objective-C example, an `autoreleasepool`
 ///	is not required because Swift expected to install one automatically.
-///	Anyway you can install one if you want to be safe.
+///	Anyway you can install one if you want to be defensive.
 autoreleasepool { () -> () in
 	let	app1		=	NSApplication.sharedApplication()	//	You need to retain the application object in stack. Otherwise it will be killed immediately.
 	let	con1		=	ExampleApplicationController()		//	So do this too...
@@ -21,6 +39,6 @@ autoreleasepool { () -> () in
 	app1.delegate	=	con1
 	app1.run()
 	
-	///	DO NOT call `NSApplicationMain`. It's handled by `NSApplication.run
+	///	DO NOT call `NSApplicationMain`. It will be handled by `NSApplication.run` method.
 	//	NSApplicationMain(C_ARGC, C_ARGV)
 }
