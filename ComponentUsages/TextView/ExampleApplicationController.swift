1846a80ad865e210c0fa203f923d6fc02a708e9d
Hoon H <drawtree@me.com>
2014-10-27 23:32:00 +0900

    Updated NSTextView example.

--- ComponentUsages/TextView/ExampleApplicationController.swift
+++ ComponentUsages/TextView/ExampleApplicationController.swift
@@ -34,9 +34,6 @@ class ExampleApplicationController: NSObject, NSApplicationDelegate {
 			
 			////
 			
-			///	It's very important to match initial size (width) to target area's size.
-			let	sz1			=	scroll1.contentSize
-			text1.frame		=	CGRect(x: 0, y: 0, width: sz1.width, height: sz1.height)
 			text1.minSize	=	CGSize(width: 0, height: 0)
 			text1.maxSize	=	CGSize(width: CGFloat.max, height: CGFloat.max)
 			
@@ -45,9 +42,12 @@ class ExampleApplicationController: NSObject, NSApplicationDelegate {
 			
 			if wordwrap {
 				///	Matching width is also important here.
+				let	sz1										=	scroll1.contentSize
+				text1.frame									=	CGRect(x: 0, y: 0, width: sz1.width, height: 0)
 				text1.textContainer!.containerSize			=	CGSize(width: sz1.width, height: CGFloat.max)
 				text1.textContainer!.widthTracksTextView	=	true
 			} else {
+//				text1.frame									=	CGRect(x: 0, y: 0, width: 0, height: 0)
 				text1.textContainer!.widthTracksTextView	=	false
 				text1.textContainer!.containerSize			=	CGSize(width: CGFloat.max, height: CGFloat.max)
 			}
@@ -66,7 +66,7 @@ class ExampleApplicationController: NSObject, NSApplicationDelegate {
 
 
 struct Resources {
-	static let		exampleText	=	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	static let		exampleText	=	"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n"
 }
 
 
