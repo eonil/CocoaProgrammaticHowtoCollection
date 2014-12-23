92820d66468e7fcf1688d359d1b1ffce178e664f
Hoon H <drawtree@me.com>
2014-12-23 11:31:21 +0900

    Added an example for borderless, round-cornered window with shadow.

---
+++ ComponentUsages/BorderlessRoundWindowWithShadow/ExampleApplicationController.swift
@@ -0,0 +1,49 @@
+//
+//  ExampleApplicationController.swift
+//  SplitViewExample
+//
+//  Created by Hoon H. on 10/12/14.
+//  Copyright (c) 2014 Eonil. All rights reserved.
+//
+
+import Cocoa
+
+
+
+class ExampleApplicationController: NSObject, NSApplicationDelegate {
+	class ExampleController {
+		
+		let	window1	=	NSWindow()
+		let	view1	=	NSView()
+		
+		init(){
+			view1.wantsLayer				=	true
+			view1.layer!.cornerRadius		=	10
+			view1.layer!.backgroundColor	=	NSColor.whiteColor().CGColor
+			
+			//
+			//	The `view1` must be configured to be round BEFORE assigned to `window1`.
+			//	Otherwise the window shadow will not work. Should be a bug in the SDK.
+			//
+			
+			window1.setFrame(CGRect(x: 400, y: 0, width: 400, height: 500), display: true)
+			window1.contentView					=	view1
+			
+			window1.backgroundColor				=	NSColor.clearColor()
+			window1.opaque						=	false
+			window1.styleMask					=	NSBorderlessWindowMask | NSResizableWindowMask
+			window1.movableByWindowBackground	=	true
+			window1.makeKeyAndOrderFront(self)
+			
+		}
+	}
+	
+	let	example1	=	ExampleController()
+}
+
+
+
+
+
+
+
