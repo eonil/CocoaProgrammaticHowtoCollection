97275bfec625a054ba3c91bb0d9fa701705d69f7
Hoon H <drawtree@gmail.com>
2016-09-25 21:44:48 +0900

    Update RegularDarkVibrantWindow to Swift3.

--- ComponentUsages/RegularDarkVibrantWindow/ExampleApplicationController.swift
+++ ComponentUsages/RegularDarkVibrantWindow/ExampleApplicationController.swift
@@ -11,50 +11,50 @@ import Cocoa
 
 
 class ExampleApplicationController: NSObject, NSApplicationDelegate {
-	
-	let	window	=	NSWindow()
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		
-		let	c1				=	NSVisualEffectView()
-		c1.material			=	NSVisualEffectMaterial.AppearanceBased
-		c1.blendingMode		=	NSVisualEffectBlendingMode.BehindWindow
-		c1.state			=	NSVisualEffectState.FollowsWindowActiveState
-		c1.needsDisplay		=	true
-		window.contentView	=	c1
-		
-		//
-		//	Don't try to make `NSBorderlessWindowMask` or `NSFullSizeContentViewWindowMask` window style
-		//	with round-cornered view in it.
-		//	It will make bad (jagged) corner rendering.
-		//	It is good for you mental health just to follow Apple's regular appearance policy.
-		
-//		window.backgroundColor	=	NSColor.clearColor()
-//		window.opaque			=	false
-//		window.styleMask		=	NSBorderlessWindowMask | NSResizableWindowMask | NSFullSizeContentViewWindowMask
-//		window.styleMask		=	NSResizableWindowMask | NSHUDWindowMask | NSMiniaturizableWindowMask | NSClosableWindowMask | NSTitledWindowMask | NSUtilityWindowMask
-		window.styleMask		=	NSResizableWindowMask | NSTitledWindowMask | NSMiniaturizableWindowMask | NSClosableWindowMask
-		window.movableByWindowBackground	=	true
-//		window.titlebarAppearsTransparent	=	true
-		window.title			=	"Regular Dark Vibrant Window"
-		window.appearance		=	NSAppearance(named: NSAppearanceNameVibrantDark)
-		
-		window.setFrame(CGRect(x: 100, y: 100, width: 800, height: 600), display: false)
-		window.makeKeyAndOrderFront(self)
-		window.invalidateShadow()
-		
-//		makeDark(window.standardWindowButton(NSWindowButton.CloseButton)!)
-//		makeDark(window.standardWindowButton(NSWindowButton.MiniaturizeButton)!)
-//		makeDark(window.standardWindowButton(NSWindowButton.ZoomButton)!)
-	}
+    
+    let window = NSWindow()
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        let c1              =   NSVisualEffectView()
+        c1.material         =    NSVisualEffectMaterial.appearanceBased
+        c1.blendingMode     =    NSVisualEffectBlendingMode.behindWindow
+        c1.state            =    NSVisualEffectState.followsWindowActiveState
+        c1.needsDisplay     =    true
+        window.contentView  =    c1
+        
+        //
+        //    Don't try to make `NSBorderlessWindowMask` or `NSFullSizeContentViewWindowMask` window style
+        //    with round-cornered view in it.
+        //    It will make bad (jagged) corner rendering.
+        //    It is good for you mental health just to follow Apple's regular appearance policy.
+        
+//        window.backgroundColor    =    NSColor.clearColor()
+//        window.opaque            =    false
+//        window.styleMask        =    NSBorderlessWindowMask | NSResizableWindowMask | NSFullSizeContentViewWindowMask
+//        window.styleMask        =    NSResizableWindowMask | NSHUDWindowMask | NSMiniaturizableWindowMask | NSClosableWindowMask | NSTitledWindowMask | NSUtilityWindowMask
+        window.styleMask = NSWindowStyleMask([.resizable, .titled, .miniaturizable, .closable])
+        window.isMovableByWindowBackground = true
+//        window.titlebarAppearsTransparent = true
+        window.title = "Regular Dark Vibrant Window"
+        window.appearance = NSAppearance(named: NSAppearanceNameVibrantDark)
+        
+        window.setFrame(CGRect(x: 100, y: 100, width: 800, height: 600), display: false)
+        window.makeKeyAndOrderFront(self)
+        window.invalidateShadow()
+        
+//        makeDark(window.standardWindowButton(NSWindowButton.CloseButton)!)
+//        makeDark(window.standardWindowButton(NSWindowButton.MiniaturizeButton)!)
+//        makeDark(window.standardWindowButton(NSWindowButton.ZoomButton)!)
+    }
 }
 
 //private func makeDark(b:NSButton) {
-//	//	CIFilter doesn't work on standard buttons. Don't know why.
-////	let	f	=	CIFilter(name: "CIColorControls")
-////	f.setValue(NSNumber(float: 1), forKey: "inputSaturation")
-////	b.layerUsesCoreImageFilters	=	true
-////	b.layer!.filters			=	[f]
-//	b.alphaValue	=	0.5
+//    //    CIFilter doesn't work on standard buttons. Don't know why.
+////    let    f    =    CIFilter(name: "CIColorControls")
+////    f.setValue(NSNumber(float: 1), forKey: "inputSaturation")
+////    b.layerUsesCoreImageFilters    =    true
+////    b.layer!.filters            =    [f]
+//    b.alphaValue    =    0.5
 //}
 
 
--- ComponentUsages/RegularDarkVibrantWindow/main.swift
+++ ComponentUsages/RegularDarkVibrantWindow/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  SplitViewExample
+//  RegularDarkVibrantWindow
 //
 //  Created by Hoon H. on 10/12/14.
 //  Copyright (c) 2014 Eonil. All rights reserved.
@@ -8,10 +8,8 @@
 
 import AppKit
 
+let app1 = NSApplication.shared() //< You need to retain the application object in stack. Otherwise it will be killed immediately.
+let con1 = ExampleApplicationController() //< So do this too...
 
-
-let	app1	=	NSApplication.sharedApplication()	//	You need to retain the application object in stack. Otherwise it will be killed immediately.
-let	con1	=	ExampleApplicationController()		//	So do this too...
-
-app1.delegate	=	con1
+app1.delegate = con1
 app1.run()
