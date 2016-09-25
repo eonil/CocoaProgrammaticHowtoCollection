f73e84843300fda0bd17afc3ea8350929c39309b
Hoon H <drawtree@gmail.com>
2016-09-25 21:19:22 +0900

    Update CustomNSTextViewWithNSTextStorage to Swift3.

--- ComponentUsages/CustomNSTextViewWithNSTextStorage/ExampleApplicationController.swift
+++ ComponentUsages/CustomNSTextViewWithNSTextStorage/ExampleApplicationController.swift
@@ -10,94 +10,50 @@ import Cocoa
 
 
 
-
-
-
-
 /////////////////////////////////////////////////////////////////////////////
 //
-//	Minimal example to customise `NSTextView` with custom `NSTextStorage`.
+//    Minimal example to customise `NSTextView` with custom `NSTextStorage`.
 //
 /////////////////////////////////////////////////////////////////////////////
 
 
 
+final class ExampleApplicationController: NSObject, NSApplicationDelegate {
+    /// If you use non-default text-storage,
+    /// the storage must be retained by something else
+    /// because `NSTextView` does not retain it.
+    /// As a consequence, your app likely to be crashed.
+    ///
+    /// Cited from manual:
+    ///
+    /// > There are two standard ways to create an object web of the
+    /// > four principal classes of the text system to handle text
+    /// > editing, layout, and display: in one case, the text view
+    /// > creates and owns the other objects; in the other case, you
+    /// > create all the objects explicitly and the text storage owns
+    /// > them.
+    ///
+    let textStorage1 = CustomTextStorage()
 
-class CustomTextView: NSTextView {
+    let window1 = NSWindow()
+    let scrollView1 = NSScrollView()
+    let textView1 = NSTextView()
 
-	//	If you use non-default text-storage, 
-	//	the storage must be retained by something else
-	//	because `NSTextView` will not retain it.
-	//	As a consequence, your app will be crashed.
-	//
-	//	Cited from manual:
-	//
-	//	>	There are two standard ways to create an object web of the
-	//	>	four principal classes of the text system to handle text 
-	//	>	editing, layout, and display: in one case, the text view
-	//	>	creates and owns the other objects; in the other case, you
-	//	>	create all the objects explicitly and the text storage owns 
-	//	>	them.
-	//
-	var	s:CustomTextStorage?
-	
-//	init() {
-//		super.init(frame: NSRect.zeroRect, textContainer: nil)
-//	}
-	
-	override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
-		super.init(frame: frameRect, textContainer: container)
-		s	=	CustomTextStorage()
-		layoutManager!.replaceTextStorage(s!)
-	}
-	
-	required init?(coder: NSCoder) {
-		super.init(coder: coder)
-		s	=	CustomTextStorage()
-		layoutManager!.replaceTextStorage(s!)
-	}
-}
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        assert(textView1.layoutManager != nil)
+        textView1.layoutManager?.replaceTextStorage(textStorage1)
 
-
-
-
-class CustomTextStorage: NSTextStorage {
-	var	s	=	""
-	
-	override var string:String {
-		get {
-			return	s
-		}
-	}
-
-	override func attributesAtIndex(location: Int, effectiveRange range: NSRangePointer) -> [String : AnyObject] {
-
-		//
-		//	`range` is an output parameter! You must set it to a proper value.
-		//
-		if range != nil {
-			range.memory	=	NSRange(location: 0, length: self.length)
-		}
-		
-		return	[NSFontAttributeName: NSFont.systemFontOfSize(24)]
-	}
-
-	override func replaceCharactersInRange(range: NSRange, withString str: String) {
-		s	=	(s as NSString).stringByReplacingCharactersInRange(range, withString: str)
-
-		//
-		//	Changes must be notified!
-		//
-		let	d	=	(str as NSString).length - range.length
-		self.edited(NSTextStorageEditActions.EditedCharacters, range: range, changeInLength: d)		//	`d` must be delta. Keep the sign.
-	}
-	override func setAttributes(attrs: [String : AnyObject]?, range: NSRange) {
-		
-		//
-		//	Changes must be notified!
-		//
-		self.edited(NSTextStorageEditActions.EditedAttributes, range: range, changeInLength: 0)
-	}
+        window1.styleMask = NSWindowStyleMask([.resizable, .closable, .miniaturizable])
+        window1.setFrame(CGRect(x: 400, y: 400, width: 400, height: 500), display: true)
+        window1.makeKeyAndOrderFront(self)
+        window1.contentView = scrollView1
+        scrollView1.documentView = textView1
+        scrollView1.hasVerticalScroller = true
+        scrollView1.hasHorizontalScroller = true
+        textView1.string = "ABC"
+        textView1.isVerticallyResizable = true
+        textView1.isHorizontallyResizable = true
+    }
 }
 
 
@@ -107,36 +63,50 @@ class CustomTextStorage: NSTextStorage {
 
 
 
-
-
-
-
-
-
-
-class ExampleApplicationController: NSObject, NSApplicationDelegate {
-	let	window1	=	NSWindow()
-	
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		window1.styleMask		|=	NSResizableWindowMask | NSClosableWindowMask | NSResizableWindowMask | NSMiniaturizableWindowMask
-		window1.setFrame(CGRect(x: 400, y: 400, width: 400, height: 500), display: true)
-		window1.makeKeyAndOrderFront(self)
-		
-		////
-		
-		let	s	=	NSScrollView()
-		let	t	=	CustomTextView()
-		
-		t.string				=	"ABC"
-		t.verticallyResizable	=	true
-		t.horizontallyResizable	=	true
-		
-		s.documentView			=	t
-		s.hasVerticalScroller	=	true
-		s.hasHorizontalScroller	=	true
-		window1.contentView		=	s
-	}
+final class CustomTextStorage: NSTextStorage {
+    var s = ""
+    override var string:String {
+        return s
+    }
+    override func attributes(at location: Int, effectiveRange range: NSRangePointer?) -> [String : Any] {
+        //
+        // `range` is an output parameter! You must set it to a proper value.
+        //
+        if range != nil {
+            range?.pointee = NSRange(location: 0, length: self.length)
+        }
+        
+        return [NSFontAttributeName: NSFont.systemFont(ofSize: 24)]
+    }
+    override func replaceCharacters(in range: NSRange, with str: String) {
+        s = (s as NSString).replacingCharacters(in: range, with: str)
+        //
+        // Changes must be notified!
+        //
+        let d = (str as NSString).length - range.length
+        self.edited(NSTextStorageEditActions.editedCharacters, range: range, changeInLength: d) //< `d` must be delta. Keep the sign.
+    }
+    override func setAttributes(_ attrs: [String : Any]?, range: NSRange) {
+        //
+        // Changes must be notified!
+        //
+        self.edited(NSTextStorageEditActions.editedAttributes, range: range, changeInLength: 0)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- ComponentUsages/CustomNSTextViewWithNSTextStorage/main.swift
+++ ComponentUsages/CustomNSTextViewWithNSTextStorage/main.swift
@@ -8,19 +8,16 @@
 
 import AppKit
 
-
-
-
-///	When compared to Objective-C example, an `autoreleasepool`
-///	is not required because Swift expected to install one automatically.
-///	Anyway you can install one if you want to be safe.
+/// When compared to Objective-C example, an `autoreleasepool`
+/// is not required because Swift expected to install one automatically.
+/// Anyway you can install one if you want to be safe.
 autoreleasepool { () -> () in
-	let	app1	=	NSApplication.sharedApplication()	//	You need to retain the application object in stack. Otherwise it will be killed immediately.
-	let	con1	=	ExampleApplicationController()		//	So do this too...
-	
-	app1.delegate	=	con1
-	app1.run()
-	
-	///	DO NOT call `NSApplicationMain`. It's handled by `NSApplication.run
-	//	NSApplicationMain(C_ARGC, C_ARGV)
+    let app1 = NSApplication.shared() //< You need to retain the application object in stack. Otherwise it will be killed immediately.
+    let con1 = ExampleApplicationController() //< So do this too...
+    
+    app1.delegate = con1
+    app1.run()
+    
+    /// DO NOT call `NSApplicationMain`. It's handled by `NSApplication.run
+    // NSApplicationMain(C_ARGC, C_ARGV)
 }
