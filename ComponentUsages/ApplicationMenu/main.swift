452d4964e4637eaaf03b954db11a895799fdf090
Hoon H <drawtree@gmail.com>
2016-09-25 20:53:58 +0900

    Update ApplicationMenu project to Swift3.

--- ComponentUsages/ApplicationMenu/main.swift
+++ ComponentUsages/ApplicationMenu/main.swift
@@ -10,48 +10,46 @@ import Cocoa
 
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let    mainMenu             =    NSMenu() //< `title` really doesn't matter here.
+        let    mainAppMenuItem      =    NSMenuItem(title: "Application", action: nil, keyEquivalent: "") //< `title` really doesn't matter here.
+        let    mainFileMenuItem     =    NSMenuItem(title: "File", action: nil, keyEquivalent: "")
+        mainMenu.addItem(mainAppMenuItem)
+        mainMenu.addItem(mainFileMenuItem)
 
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
+        let    appMenu              =    NSMenu() //< `title` really doesn't matter here.
+        mainAppMenuItem.submenu     =    appMenu
 
-		let	mainMenu		=	NSMenu()									//	`title` really doesn't matter.
-		let	mainAppMenuItem		=	NSMenuItem(title: "Application", action: nil, keyEquivalent: "")		//	`title` really doesn't matter.
-		let	mainFileMenuItem	=	NSMenuItem(title: "File", action: nil, keyEquivalent: "")
-		mainMenu.addItem(mainAppMenuItem)
-		mainMenu.addItem(mainFileMenuItem)
+        let    appServicesMenu      =    NSMenu()
+        NSApp.servicesMenu          =    appServicesMenu
+        appMenu.addItem(withTitle: "About Me", action: nil, keyEquivalent: "")
+        appMenu.addItem(NSMenuItem.separator())
+        appMenu.addItem(withTitle: "Preferences...", action: nil, keyEquivalent: ",")
+        appMenu.addItem(NSMenuItem.separator())
+        appMenu.addItem(withTitle: "Hide Me", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
+        appMenu.addItem({ ()->NSMenuItem in
+            let m = NSMenuItem(title: "Hide Others", action: #selector(NSApplication.hideOtherApplications(_:)), keyEquivalent: "h")
+            m.keyEquivalentModifierMask = NSEventModifierFlags([.command, .option])
+            return m
+            }())
+        appMenu.addItem(withTitle: "Show All", action: #selector(NSApplication.unhideAllApplications(_:)), keyEquivalent: "")
 
-		let	appMenu			=	NSMenu()									//	`title` really doesn't matter.
-		mainAppMenuItem.submenu		=	appMenu
+        appMenu.addItem(NSMenuItem.separator())
+        appMenu.addItem(withTitle: "Services", action: nil, keyEquivalent: "").submenu    =    appServicesMenu
+        appMenu.addItem(NSMenuItem.separator())
+        appMenu.addItem(withTitle: "Quit Me", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
 
-		let	appServicesMenu		=	NSMenu()
-		NSApp.servicesMenu		=	appServicesMenu
-		appMenu.addItemWithTitle("About Me", action: nil, keyEquivalent: "")
-		appMenu.addItem(NSMenuItem.separatorItem())
-		appMenu.addItemWithTitle("Preferences...", action: nil, keyEquivalent: ",")
-		appMenu.addItem(NSMenuItem.separatorItem())
-		appMenu.addItemWithTitle("Hide Me", action: "hide:", keyEquivalent: "h")
-		appMenu.addItem({ ()->NSMenuItem in
-			let m	=	NSMenuItem(title: "Hide Others", action: "hideOtherApplications:", keyEquivalent: "h")
-			m.keyEquivalentModifierMask	=	Int(NSEventModifierFlags([.CommandKeyMask, .AlternateKeyMask]).rawValue)
-			return	m
-			}())
-		appMenu.addItemWithTitle("Show All", action: "unhideAllApplications:", keyEquivalent: "")
+        let    fileMenu             =    NSMenu(title: "File")
+        mainFileMenuItem.submenu    =    fileMenu
+        fileMenu.addItem(withTitle: "New...", action: #selector(NSDocumentController.newDocument(_:)), keyEquivalent: "n")
 
-		appMenu.addItem(NSMenuItem.separatorItem())
-		appMenu.addItemWithTitle("Services", action: nil, keyEquivalent: "")!.submenu	=	appServicesMenu
-		appMenu.addItem(NSMenuItem.separatorItem())
-		appMenu.addItemWithTitle("Quit Me", action: "terminate:", keyEquivalent: "q")
-
-		let	fileMenu		=	NSMenu(title: "File")
-		mainFileMenuItem.submenu	=	fileMenu
-		fileMenu.addItemWithTitle("New...", action: "newDocument:", keyEquivalent: "n")
-
-		NSApplication.sharedApplication().mainMenu		=	mainMenu
-	}
+        NSApplication.shared().mainMenu = mainMenu
+    }
 
 }
 
 autoreleasepool {
-	let	del	=	AppDelegate()
-	NSApplication.sharedApplication().delegate	=	del
-	NSApplication.sharedApplication().run()
+    let del = AppDelegate()
+    NSApplication.shared().delegate = del
+    NSApplication.shared().run()
 }
