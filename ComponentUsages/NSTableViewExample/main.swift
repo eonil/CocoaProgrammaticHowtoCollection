71dbdef57105d3962a32c39eda1291478f86382c
Hoon H <drawtree@gmail.com>
2016-09-25 21:23:54 +0900

    Update NSTableViewExample to Swift3.

--- ComponentUsages/NSTableViewExample/main.swift
+++ ComponentUsages/NSTableViewExample/main.swift
@@ -8,52 +8,51 @@
 
 import AppKit
 
-
 @objc
 final class ExampleTableViewController : NSObject, NSTableViewDataSource, NSTableViewDelegate {
-	
-	let	scroll		=	NSScrollView()
-	let	table		=	NSTableView()
-	
-	override init() {
-		super.init()
-		
-		scroll.documentView		=	table
-		scroll.hasVerticalScroller	=	true
-		
-		let	tc1	=	NSTableColumn(identifier: "NAME_COLUMN")
-		tc1.title	=	"Name"
-		table.addTableColumn(tc1)
-		table.setDelegate(self)
-		table.setDataSource(self)
-		table.rowSizeStyle	=	NSTableViewRowSizeStyle.Small
-	}
-	
-	@objc
-	func numberOfRowsInTableView(tableView: NSTableView) -> Int {
-		return	1000
-	}
-	
-	@objc
-	func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
-		let	VIEW_ID1	=	"VIEW_ID1"
-		if let v1 = tableView.makeViewWithIdentifier(VIEW_ID1, owner: nil) {
-			print("Reused \(ObjectIdentifier(v1))")
-			return	v1
-		} else {
-			let	v1	=	NSTextField()	//	Don't forget that you also can use `NSTableCellView` class for view-based tables.
-			v1.identifier	=	VIEW_ID1	//	This is essential!
-			return	v1
-		}
-	}
-//	@objc
-//	func tableView(tableView: NSTableView, didAddRowView rowView: NSTableRowView, forRow row: Int) {
-//		rowViewPool.remove(ObjectBox(object: rowView))
-//	}
-//	@objc
-//	func tableView(tableView: NSTableView, didRemoveRowView rowView: NSTableRowView, forRow row: Int) {
-//		rowViewPool.insert(ObjectBox(object: rowView))
-//	}
+
+    let scroll = NSScrollView()
+    let table = NSTableView()
+
+    override init() {
+        super.init()
+
+        scroll.documentView = table
+        scroll.hasVerticalScroller = true
+
+        let tc1 = NSTableColumn(identifier: "NAME_COLUMN")
+        tc1.title = "Name"
+        table.addTableColumn(tc1)
+        table.delegate = self
+        table.dataSource = self
+        table.rowSizeStyle = NSTableViewRowSizeStyle.small
+    }
+
+    @objc
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 1000
+    }
+
+    @objc
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let VIEW_ID1 = "VIEW_ID1"
+        if let v1 = tableView.make(withIdentifier: VIEW_ID1, owner: nil) {
+            print("Reused \(ObjectIdentifier(v1))")
+            return v1
+        } else {
+            let v1 = NSTextField() //< Don't forget that you also can use `NSTableCellView` class for view-based tables.
+            v1.identifier = VIEW_ID1 //< This is essential!
+            return v1
+        }
+    }
+    //    @objc
+    //    func tableView(tableView: NSTableView, didAddRowView rowView: NSTableRowView, forRow row: Int) {
+    //        rowViewPool.remove(ObjectBox(object: rowView))
+    //    }
+    //    @objc
+    //    func tableView(tableView: NSTableView, didRemoveRowView rowView: NSTableRowView, forRow row: Int) {
+    //        rowViewPool.insert(ObjectBox(object: rowView))
+    //    }
 }
 
 
@@ -68,24 +67,24 @@ final class ExampleTableViewController : NSObject, NSTableViewDataSource, NSTabl
 
 
 
-///	MARK:
+///    MARK:
 final class ApplicationController: NSObject, NSApplicationDelegate {
-	let	controller1	=	ExampleTableViewController()
-	let	window1		=	NSWindow()
-	
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		window1.contentView		=	controller1.scroll
-		window1.styleMask		|=	NSResizableWindowMask | NSTitledWindowMask
-		window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: false)
-		window1.makeKeyAndOrderFront(self)
-	}
+    let controller1 = ExampleTableViewController()
+    let window1 = NSWindow()
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window1.contentView = controller1.scroll
+        window1.styleMask = NSWindowStyleMask([.resizable, .titled])
+        window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: false)
+        window1.makeKeyAndOrderFront(self)
+    }
 }
 
 autoreleasepool { () -> () in
-	let	app1		=	NSApplication.sharedApplication()
-	let	con1		=	ApplicationController()
-	
-	app1.delegate	=	con1
-	app1.run()
+    let app1 = NSApplication.shared()
+    let con1 = ApplicationController()
+    
+    app1.delegate = con1
+    app1.run()
 }
 
