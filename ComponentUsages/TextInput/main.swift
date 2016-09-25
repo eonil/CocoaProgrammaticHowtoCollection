341191d81e828356653e7f50d3c02c42f6e0c146
Hoon H <drawtree@gmail.com>
2016-09-25 21:50:17 +0900

    Update TextInput to Swift3.

--- ComponentUsages/TextInput/main.swift
+++ ComponentUsages/TextInput/main.swift
@@ -8,98 +8,94 @@
 
 import Cocoa
 
-
-
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//	Note
-//	----
-//	`NSTextInputClient` need to be implemented by a `NSView`. 
-//	Having separated dedicated object for delegation doesn't work. (I don't know how to make it work)
+// Note
+// ----
+// `NSTextInputClient` need to be implemented by a `NSView`. 
+// Having separated dedicated object for delegation doesn't work. (I don't know how to make it work)
+//
+// This also fully support IME. Try input Korean text.
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
 
 final class ExampleView: NSView, NSTextInputClient {
-	var	_buffer				=	NSMutableString()
-	var	_markedRange		=	nil as Range<UTF16Index>?
-	
-	//	Essential methods.
-	//	You need to make some proper selection management for your application.
+    var _buffer = NSMutableString()
+    var _markedRange = nil as CountableRange<UTF16Index>?
+    
+    // Essential methods.
+    // You need to make some proper selection management for your application.
 
-	func selectedRange() -> NSRange {
-		return	NSRange(location: _buffer.length, length: 0)
-	}
-	func markedRange() -> NSRange {
-		return	NSRange.fromUTF16Range(_markedRange)
-	}
-	func hasMarkedText() -> Bool {
-		return	_markedRange != nil
-	}
-	
-	func insertText(aString: AnyObject, replacementRange: NSRange) {
-		let	s	=	findNSStringObject(aString)
-		let	r	=	replacementRange.location != NSNotFound ? replacementRange : (_markedRange != nil ? self.markedRange() : self.selectedRange())
-		
-		_buffer.replaceCharactersInRange(r, withString: s as String)
-		self.unmarkText()
-		self.inputContext!.invalidateCharacterCoordinates()
-		
-		Swift.print("insertText, current buffer = \(_buffer)")
-	}
-	func setMarkedText(aString: AnyObject, selectedRange: NSRange, replacementRange: NSRange) {
-		let	s	=	findNSStringObject(aString)
-		let	r	=	replacementRange.location != NSNotFound ? replacementRange : (_markedRange != nil ? self.markedRange() : self.selectedRange())
-		
-		_buffer.replaceCharactersInRange(r, withString: s as String)
-		_markedRange	=	r.location..<(r.location+s.length)
-			
-		Swift.print("setMarkedText, current buffer = \(_buffer)")
-	}
-	func unmarkText() {
-		_markedRange	=	nil
-		self.inputContext!.discardMarkedText()
-		
-		Swift.print("unmarkText, current buffer = \(_buffer)")
-	}
-	
-	
-	
-	///	Non-essential methods, but required.
-	func attributedSubstringForProposedRange(aRange: NSRange, actualRange: NSRangePointer) -> NSAttributedString? {
-		return	nil
-	}
-	func validAttributesForMarkedText() -> [String] {
-		return	[]
-	}
-	func firstRectForCharacterRange(aRange: NSRange, actualRange: NSRangePointer) -> NSRect {
-		return	CGRect.zero
-	}
-	func characterIndexForPoint(aPoint: NSPoint) -> Int {
-		return	NSNotFound
-	}
+    func selectedRange() -> NSRange {
+        return NSRange(location: _buffer.length, length: 0)
+    }
+    func markedRange() -> NSRange {
+        return NSRange.fromUTF16Range(_markedRange)
+    }
+    func hasMarkedText() -> Bool {
+        return _markedRange != nil
+    }
+    
+    func insertText(_ aString: Any, replacementRange: NSRange) {
+        let s = findNSStringObject(aString as AnyObject)
+        let r = replacementRange.location != NSNotFound ? replacementRange : (_markedRange != nil ? self.markedRange() : self.selectedRange())
+        
+        _buffer.replaceCharacters(in: r, with: s as String)
+        self.unmarkText()
+        self.inputContext!.invalidateCharacterCoordinates()
+        
+        Swift.print("insertText, current buffer = \(_buffer)")
+    }
+    func setMarkedText(_ aString: Any, selectedRange: NSRange, replacementRange: NSRange) {
+        let s = findNSStringObject(aString as AnyObject)
+        let r = replacementRange.location != NSNotFound ? replacementRange : (_markedRange != nil ? self.markedRange() : self.selectedRange())
+        
+        _buffer.replaceCharacters(in: r, with: s as String)
+        _markedRange = r.location..<(r.location+s.length)
+            
+        Swift.print("setMarkedText, current buffer = \(_buffer)")
+    }
+    func unmarkText() {
+        _markedRange = nil
+        self.inputContext!.discardMarkedText()
+        
+        Swift.print("unmarkText, current buffer = \(_buffer)")
+    }
+    
+    
+    
+    /// Non-essential methods, but required.
+    func attributedSubstring(forProposedRange aRange: NSRange, actualRange: NSRangePointer?) -> NSAttributedString? {
+        return nil
+    }
+    func validAttributesForMarkedText() -> [String] {
+        return []
+    }
+    func firstRect(forCharacterRange aRange: NSRange, actualRange: NSRangePointer?) -> NSRect {
+        return CGRect.zero
+    }
+    func characterIndex(for aPoint: NSPoint) -> Int {
+        return NSNotFound
+    }
 
-	
-	
-	//	Boilerplates, and required.
-	//	If you don't override these methods, the view will not accept any keyboard input.
-	override var acceptsFirstResponder:Bool {
-		get {
-			return	true
-		}
-	}
-	override func becomeFirstResponder() -> Bool {
-		return	true
-	}
-	override func resignFirstResponder() -> Bool {
-		return	true
-	}
-	override func keyDown(theEvent: NSEvent) {
-		self.inputContext!.handleEvent(theEvent)
-	}
+    
+    
+    // Boilerplates, and required.
+    // If you don't override these methods, the view will not accept any keyboard input.
+    override var acceptsFirstResponder:Bool {
+        get {
+            return true
+        }
+    }
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    override func resignFirstResponder() -> Bool {
+        return true
+    }
+    override func keyDown(with theEvent: NSEvent) {
+        self.inputContext!.handleEvent(theEvent)
+    }
 }
 
 
@@ -139,49 +135,49 @@ final class ExampleView: NSView, NSTextInputClient {
 
 
 
-///	MARK:
-///	MARK:	Boilerplates
+/// MARK:
+/// MARK:    Boilerplates
 
-typealias	UTF16Index	=	Int
+typealias    UTF16Index    =    Int
 
 extension NSRange {
-	static func fromUTF16Range(range:Range<UTF16Index>?) -> NSRange {
-		if let r = range {
-			return	NSRange(location: r.startIndex, length: r.endIndex - r.startIndex)
-		}
-		return	NSRange(location: NSNotFound, length: 0)
-	}
+    static func fromUTF16Range(_ range:CountableRange<UTF16Index>?) -> NSRange {
+        if let r = range {
+            return    NSRange(location: r.lowerBound, length: r.upperBound - r.lowerBound)
+        }
+        return    NSRange(location: NSNotFound, length: 0)
+    }
 }
 
-func findNSStringObject(o:AnyObject) -> NSString {
-	if o is NSString {
-		return	o as! NSString
-	}
-	if o is NSAttributedString {
-		return	(o as! NSAttributedString).string
-	}
-	fatalError()
+func findNSStringObject(_ o:AnyObject) -> NSString {
+    if o is NSString {
+        return    o as! NSString
+    }
+    if o is NSAttributedString {
+        return    (o as! NSAttributedString).string as NSString
+    }
+    fatalError()
 }
 
 
 
 
 final class BoilerplateApplicationController: NSObject, NSApplicationDelegate {
-	let	window1	=	NSWindow()
-	let	view1	=	ExampleView()
-	
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: true)
-		window1.makeKeyAndOrderFront(self)
-		window1.contentView	=	view1
-	}
+    let    window1    =    NSWindow()
+    let    view1    =    ExampleView()
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: true)
+        window1.makeKeyAndOrderFront(self)
+        window1.contentView    =    view1
+    }
 }
 
 
-let	app1		=	NSApplication.sharedApplication()
-let	con1		=	BoilerplateApplicationController()
+let    app1        =    NSApplication.shared()
+let    con1        =    BoilerplateApplicationController()
 
-app1.delegate	=	con1
+app1.delegate    =    con1
 app1.run()
 
 
