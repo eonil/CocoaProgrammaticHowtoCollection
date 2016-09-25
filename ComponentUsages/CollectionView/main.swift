0b171f52b04df7eb4aca1a50306f90060367d891
Hoon H <drawtree@gmail.com>
2016-09-25 21:03:59 +0900

    Update CollectionView to Swift3.

--- ComponentUsages/CollectionView/main.swift
+++ ComponentUsages/CollectionView/main.swift
@@ -8,43 +8,41 @@
 
 import AppKit
 
-
-
 final class ExampleApplicationController: NSObject, NSApplicationDelegate {
-	
-	///	Seems fine to create AppKit UI classes before `NSApplication` object
-	///	to be created starting OSX 10.10. (it was an error in OSX 10.9)
-	let	window1	=	NSWindow()
-	
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: true)
-		window1.makeKeyAndOrderFront(self)
-		
-		////
-		
-		let	sv	=	NSScrollView()
-		let	cv	=	NSCollectionView()
-		
-		window1.contentView	=	sv
-		sv.documentView		=	cv
-		cv.itemPrototype	=	ExampleItemViewController()
-		cv.content	=	["AAA", "BBB", "CCC"]
-	}
+
+    /// Seems fine to create AppKit UI classes before `NSApplication` object
+    /// to be created starting OSX 10.10. (it was an error in OSX 10.9)
+    let window1 = NSWindow()
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        window1.setFrame(CGRect(x: 0, y: 0, width: 800, height: 500), display: true)
+        window1.makeKeyAndOrderFront(self)
+
+        ////
+
+        let sv = NSScrollView()
+        let cv = NSCollectionView()
+
+        window1.contentView =   sv
+        sv.documentView     =   cv
+        cv.itemPrototype    =   ExampleItemViewController()
+        cv.content          =   ["AAA", "BBB", "CCC"]
+    }
 }
 
 final class ExampleItemViewController: NSCollectionViewItem {
-	override func loadView() {
-		super.view	=	NSTextField()
-	}
-	
-	override var representedObject:AnyObject? {
-		get {
-			return	(self.view as! NSTextField).objectValue
-		}
-		set(v) {
-			(self.view as! NSTextField).objectValue	=	v
-		}
-	}
+    override func loadView() {
+        super.view = NSTextField()
+    }
+
+    override var representedObject: Any? {
+        get {
+            return (self.view as! NSTextField).objectValue as AnyObject?
+        }
+        set {
+            (self.view as! NSTextField).objectValue = newValue
+        }
+    }
 }
 
 
@@ -56,8 +54,8 @@ final class ExampleItemViewController: NSCollectionViewItem {
 
 
 
-let	app1		=	NSApplication.sharedApplication()
-let	con1		=	ExampleApplicationController()
+let app1 = NSApplication.shared()
+let con1 = ExampleApplicationController()
 
-app1.delegate	=	con1
+app1.delegate = con1
 app1.run()
