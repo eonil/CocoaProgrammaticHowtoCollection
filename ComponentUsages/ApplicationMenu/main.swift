2a2611cc751a1a7784570efae57e86adf40c29bb
Hoon H <drawtree@gmail.com>
2016-11-05 13:23:32 +0900

    ApplicationMenu: Clease tabs into spaces. Add some important comments.

--- ComponentUsages/ApplicationMenu/main.swift
+++ ComponentUsages/ApplicationMenu/main.swift
@@ -8,48 +8,53 @@
 
 import Cocoa
 
-
+/// You SHOULD NOT use `@NSApplicationMain`
+/// to make your custom menu to work.
 class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let    mainMenu             =    NSMenu() //< `title` really doesn't matter here.
-        let    mainAppMenuItem      =    NSMenuItem(title: "Application", action: nil, keyEquivalent: "") //< `title` really doesn't matter here.
-        let    mainFileMenuItem     =    NSMenuItem(title: "File", action: nil, keyEquivalent: "")
-        mainMenu.addItem(mainAppMenuItem)
-        mainMenu.addItem(mainFileMenuItem)
-
-        let    appMenu              =    NSMenu() //< `title` really doesn't matter here.
-        mainAppMenuItem.submenu     =    appMenu
-
-        let    appServicesMenu      =    NSMenu()
-        NSApp.servicesMenu          =    appServicesMenu
-        appMenu.addItem(withTitle: "About Me", action: nil, keyEquivalent: "")
-        appMenu.addItem(NSMenuItem.separator())
-        appMenu.addItem(withTitle: "Preferences...", action: nil, keyEquivalent: ",")
-        appMenu.addItem(NSMenuItem.separator())
-        appMenu.addItem(withTitle: "Hide Me", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
-        appMenu.addItem({ ()->NSMenuItem in
-            let m = NSMenuItem(title: "Hide Others", action: #selector(NSApplication.hideOtherApplications(_:)), keyEquivalent: "h")
-            m.keyEquivalentModifierMask = NSEventModifierFlags([.command, .option])
-            return m
-            }())
-        appMenu.addItem(withTitle: "Show All", action: #selector(NSApplication.unhideAllApplications(_:)), keyEquivalent: "")
-
-        appMenu.addItem(NSMenuItem.separator())
-        appMenu.addItem(withTitle: "Services", action: nil, keyEquivalent: "").submenu    =    appServicesMenu
-        appMenu.addItem(NSMenuItem.separator())
-        appMenu.addItem(withTitle: "Quit Me", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
-
-        let    fileMenu             =    NSMenu(title: "File")
-        mainFileMenuItem.submenu    =    fileMenu
-        fileMenu.addItem(withTitle: "New...", action: #selector(NSDocumentController.newDocument(_:)), keyEquivalent: "n")
-
-        NSApplication.shared().mainMenu = mainMenu
-    }
-
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
+    func applicationWillTerminate(_ aNotification: Notification) {}
 }
 
-autoreleasepool {
-    let del = AppDelegate()
-    NSApplication.shared().delegate = del
-    NSApplication.shared().run()
+func makeMainMenu() -> NSMenu {
+    let mainMenu            = NSMenu() // `title` really doesn't matter.
+    let mainAppMenuItem     = NSMenuItem(title: "Application", action: nil, keyEquivalent: "") // `title` really doesn't matter.
+    let mainFileMenuItem    = NSMenuItem(title: "File", action: nil, keyEquivalent: "")
+    mainMenu.addItem(mainAppMenuItem)
+    mainMenu.addItem(mainFileMenuItem)
+
+    let appMenu             = NSMenu() // `title` really doesn't matter.
+    mainAppMenuItem.submenu = appMenu
+
+    let appServicesMenu     = NSMenu()
+    NSApp.servicesMenu      = appServicesMenu
+
+    appMenu.addItem(withTitle: "About Me", action: nil, keyEquivalent: "")
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Preferences...", action: nil, keyEquivalent: ",")
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Hide Me", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
+    appMenu.addItem({ () -> NSMenuItem in
+        let m = NSMenuItem(title: "Hide Others", action: #selector(NSApplication.hideOtherApplications(_:)), keyEquivalent: "h")
+        m.keyEquivalentModifierMask = [.command, .option]
+        return m
+        }())
+    appMenu.addItem(withTitle: "Show All", action: #selector(NSApplication.unhideAllApplications(_:)), keyEquivalent: "")
+
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Services", action: nil, keyEquivalent: "").submenu = appServicesMenu
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Quit Me", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+
+    let fileMenu = NSMenu(title: "File")
+    mainFileMenuItem.submenu = fileMenu
+    fileMenu.addItem(withTitle: "New...", action: #selector(NSDocumentController.newDocument(_:)), keyEquivalent: "n")
+
+    return mainMenu
 }
+
+let del = AppDelegate()
+/// Setting main menu MUST be done before you setting app delegate.
+/// I don't know why.
+NSApplication.shared().mainMenu = makeMainMenu()
+NSApplication.shared().delegate = del
+NSApplication.shared().run()
